5a9af1c85f63c73af905df8b2744852e181ba308
Sina Yousefi <yousefisina9@yahoo.com>
2025-05-27 22:32:10 +0200

    Fix client component issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { motion } from 'framer-motion'
 import { WrenchScrewdriverIcon, BeakerIcon, ChartBarIcon } from '@heroicons/react/24/outline'
 import PhotoGallery from './components/PhotoGallery'
